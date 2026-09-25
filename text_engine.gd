8a6fb046036985df0deadf47a4ef079befb2cf96
Yev Nosenko <yevhenii2208@gmail.com>
2026-09-25 18:49:52 -0400

    Started testing the letter node to make sure I can use it inside the text engine.

--- text_engine.gd
+++ text_engine.gd
@@ -2,7 +2,10 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 @onready var letters = $letters
 var letter: PackedScene = preload("res://letter.tscn")
-var alphabet_t = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","?","sf","ff",]
+var alphabet_t = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+"q","r","s","t","u","v","w","x","y","z","!","?","sf","ff"]
+var alphabet_p = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+"q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","!","?","/","|","@","#","$","*","(",")","'"]
 var sts = "" # sentence to spell
 var space = 60 # pixels between chars. ~60 pixels between words (space is technically a char).
 var frame = -1
@@ -16,8 +19,14 @@ func _process(delta: float) -> void:
 	if frame % 10 == 1:
 		var new_letter = letter.instantiate() as AnimatedSprite2D
 		new_letter.global_position = Vector2(pos.x * frame, pos.y)
-		var luck = rng.randi_range(0,29)
 		letters.add_child(new_letter)
+		var luck = rng.randi_range(0,1)
+		if luck == 1:
+			luck = rng.randi_range(0,29)
+		else:
+			luck = rng.randi_range(0,64)
+		
+		
 		new_letter.set_let(luck)
 	
 func spawn_let() -> void:
